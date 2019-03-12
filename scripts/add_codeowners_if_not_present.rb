a11c6c761e57a197195eeb05da89d6cc06a44c49
John Roesler <johnrroesler@gmail.com>
2019-03-12 13:32:11 -0500

    cookstyle

--- scripts/add_codeowners_if_not_present.rb
+++ scripts/add_codeowners_if_not_present.rb
@@ -7,18 +7,18 @@ uri = URI(url)
 req = Net::HTTP::Get.new(uri)
 req.basic_auth(ARGV[0], ARGV[1])
 http = Net::HTTP.new(uri.hostname, uri.port)
-http.use_ssl = (uri.scheme == "https")
+http.use_ssl = (uri.scheme == 'https')
 resp = http.request(req)
 parsed_response = JSON.parse(resp.body)
 
 teams = []
 parsed_response.each do |pr|
-  teams<<pr['name']
+  teams << pr['name']
 end
 
 Dir.glob('*').each do |dir|
   next unless teams.include?(dir)
-  Dir.mkdir "#{Dir.pwd}/#{dir}/.github" unless File.exists?("#{Dir.pwd}/#{dir}/.github")
-  f =File.open("#{Dir.pwd}/#{dir}/.github/CODEOWNERS", "w")
+  Dir.mkdir "#{Dir.pwd}/#{dir}/.github" unless File.exist?("#{Dir.pwd}/#{dir}/.github")
+  f = File.open("#{Dir.pwd}/#{dir}/.github/CODEOWNERS", 'w')
   f.puts "* sous-chefs/#{dir}"
 end
--- scripts/watch_repos.rb
+++ scripts/watch_repos.rb
@@ -6,12 +6,11 @@ Dir.glob('*').each do |dir|
   sleep 2
   puts "Setting a watch subscription on #{dir}"
   pr_uri = URI("https://api.github.com/repos/sous-chefs/#{dir}/subscription")
-  pr_req = Net::HTTP::Put.new(pr_uri, {'Content-Type': 'text/json'})
+  pr_req = Net::HTTP::Put.new(pr_uri, 'Content-Type': 'text/json')
   pr_req.basic_auth(ARGV[0], ARGV[1])
-  pr_req.body = {
-    "subscribed": true}.to_json
+  pr_req.body = { 'subscribed': true }.to_json
   pr_http = Net::HTTP.new(pr_uri.hostname, pr_uri.port)
-  pr_http.use_ssl = (pr_uri.scheme == "https")
+  pr_http.use_ssl = (pr_uri.scheme == 'https')
   pr_resp = pr_http.request(pr_req)
   puts pr_resp.code
 end
