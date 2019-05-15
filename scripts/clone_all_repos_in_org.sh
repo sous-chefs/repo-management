310856392893aa054d837e17002d1fc766697bcc
Xorima <4923914+Xorima@users.noreply.github.com>
2019-05-15 03:25:37 +0100

    Update clone_all_repos_in_org.sh (#12)

--- scripts/clone_all_repos_in_org.sh
+++ scripts/clone_all_repos_in_org.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://api.github.com/orgs/sous-chefs/repos\?per_page\=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+curl -s https://api.github.com/orgs/sous-chefs/repos\?per_page\=200 | ruby -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
