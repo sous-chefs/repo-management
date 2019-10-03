36fd9aaf9b5d947fb8b04176ebd533bbefd891a9
Jason Field <jason@avon-lea.co.uk>
2019-10-03 08:34:47 +0100

    ruby linting fixes

--- scripts/circleci_maker.rb
+++ scripts/circleci_maker.rb
@@ -5,54 +5,53 @@ require 'json'
 dokken_instances = JSON.load(ARGF.read)
 
 circle_config = {
-  "lint_and_unit" => [
-    "delivery",
-    "danger",
-    "lint-yaml",
-    "lint-markdown",
-  ],
-  "version" => 2.1,
-  "orbs" => {
-    "kitchen" => "sous-chefs/kitchen@2"
+  'lint_and_unit' => %w(
+    delivery
+    danger
+    lint-yaml
+    lint-markdown
+  ),
+  'version' => 2.1,
+  'orbs' => {
+    'kitchen' => 'sous-chefs/kitchen@2',
   },
-  "workflows" => {
-    "kitchen" => {
-      "jobs" => [
+  'workflows' => {
+    'kitchen' => {
+      'jobs' => [
         {
-          "kitchen/yamllint" => {
-            "name" => "lint-yaml"
-          }
+          'kitchen/yamllint' => {
+            'name' => 'lint-yaml',
+          },
         },
         {
-          "kitchen/mdlint" => {
-            "name" => "lint-markdown"
-          }
+          'kitchen/mdlint' => {
+            'name' => 'lint-markdown',
+          },
         },
         {
-          "kitchen/danger" => {
-            "name" => "danger",
-            "context" => "Danger"
-          }
+          'kitchen/danger' => {
+            'name' => 'danger',
+            'context' => 'Danger',
+          },
         },
         {
-          "kitchen/delivery" => {
-            "name" => "delivery"
-          }
-        }
-      ]
-    }
-  }
+          'kitchen/delivery' => {
+            'name' => 'delivery',
+          },
+        },
+      ],
+    },
+  },
 }
 
 # Handle docker instances
-instances = [] 
 dokken_instances.each do |i|
   instance_name = i['instance']
-  circle_config['workflows']['kitchen']['jobs'].append({'kitchen/dokken-single' => {
+  circle_config['workflows']['kitchen']['jobs'].append('kitchen/dokken-single' => {
     'name' => instance_name,
     'suite' => instance_name,
-    "requires" => "*lint_and_unit"
-  }})
+    'requires' => '*lint_and_unit',
+  })
 end
 yaml_config = circle_config.to_yaml
 # The & is not part of the standard, so we have to hack it in
